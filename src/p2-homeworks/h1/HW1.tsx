62845cc60cb3dbde12e9733db1f0a8c1ff86984c
m1rade <forx98@gmail.com>
2022-07-29 21:06:41 +0300

    Changes

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Message from "./Message";
+import AlternativeMessage from "./AlternativeMessage";
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
@@ -25,7 +26,7 @@ function HW1() {
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
+            <AlternativeMessage/>
             <hr/>
         </div>
     )
