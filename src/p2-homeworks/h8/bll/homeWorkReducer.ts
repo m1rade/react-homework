018456e270be1cc24ed25e16360aa47d80e672c5
m1rade <forx98@gmail.com>
2022-09-15 16:17:37 +0300

    HW8 fixed

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,26 +1,38 @@
-import {UserType} from "../HW8";
+import { UserType } from "../HW8";
 
 type ActionType = {
-    type: "sort" | "check",
-    payload: "up" | "down" | 18
+    type: "sort" | "check";
+    payload: "up" | "down" | 18;
 };
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+export const homeWorkReducer = (
+    state: UserType[],
+    action: ActionType
+): UserType[] => {
+    // need to fix any
     switch (action.type) {
         case "sort": {
             // need to fix
+            const stateCopy = [...state].sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1;
+                } else if (a.name < b.name) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
             if (action.payload === "up") {
-                return [...state].sort((a, b) => a.name > b.name ? 1 : -1);
-            } else if (action.payload === "down") {
-                return [...state].sort((a, b) => a.name < b.name ? 1 : -1);
+                return stateCopy;
+            } else {
+                return stateCopy.reverse();
             }
-            return state;
         }
         case "check": {
             // need to fix
-            return state.filter(s => s.age > action.payload)
+            return state.filter((s) => s.age > action.payload);
         }
         default:
-            return state
+            return state;
     }
-}
+};
